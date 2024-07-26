edfa174281f83859eedc56214be5f155ab1f5eb7
Sanish <sanishpagui162@gmail.com>
2024-07-26 21:15:13 +0530

    Arigato

---
+++ Assignments/Assignment2/No_1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,vote,count=0,spoilt_ballot=0;
+    cout<<"Enter the number of people voted: ";
+    cin>>n;
+    cout<<endl; 
+    for(int i=0;i<n;i++){
+        cout<<"Voter "<<i+1<<",Enter the candidate number between (1-5):";
+        cin>>vote;
+        if(vote<0 || vote>5)
+            spoilt_ballot++;
+        else
+            count++;
+    }
+    cout<<"\nThe total number of valid ballots is: "<<count<<endl;
+    cout<<"The total number of spoilt ballets is "<<spoilt_ballot;
+}
---
+++ Assignments/Assignment2/No_2.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+class Cricketers{
+    string name;
+    int runs;
+    int innings;
+    int not_out;
+    float avg;
+    public:
+    Cricketers(string nam,int r,int i,int no){
+        name=nam;
+        runs=r;
+        innings=i;
+        not_out=no;
+    }
+    void calculateAvg(){
+        avg=runs/(innings-not_out);
+    }
+    void display(){
+        cout<<setw(20)<<left<<name;
+        cout<<setw(20)<<left<<runs;
+        cout<<setw(20)<<left<<innings;
+        cout<<setw(20)<<left<<not_out;
+        cout<<setw(20)<<left<<avg<<endl;
+    }
+};
+void header(){
+    cout<<setw(20)<<left<<"Player's name";
+    cout<<setw(20)<<left<<"Runs";
+    cout<<setw(20)<<left<<"Innings";
+    cout<<setw(20)<<left<<"Times not out";
+    cout<<setw(20)<<left<<"Average"<<endl;
+}
+int main(){
+    Cricketers c1("Sachin",8430,230,18);
+    Cricketers c2("Saurav",4200,130,9);
+    Cricketers c3("Rahul",3350,105,11);
+    c1.calculateAvg();
+    c2.calculateAvg();
+    c3.calculateAvg();
+    header();
+    c1.display();
+    c2.display();
+    c3.display();
+}
